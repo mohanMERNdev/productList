ec9730e920e4aa238694b15414d3b23609a77e05
mohanMERNdev <mohanjayabal5@gmail.com>
2024-10-20 18:49:44 +0530

    comm

---
+++ src/components/Product/index.js
@@ -0,0 +1,405 @@
+import {Component} from 'react'
+import './index.css'
+import TabItem from '../Tabs'
+import AppItem from '../ProductList'
+
+const tabsList = [
+  {tabId: 'SOCIAL', displayText: 'Social'},
+  {tabId: 'GAMES', displayText: 'Games'},
+  {tabId: 'NEWS', displayText: 'News'},
+  {tabId: 'FOOD', displayText: 'Food'},
+]
+
+const appsList = [
+  {
+    appId: 0,
+    appName: 'Facebook',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-facebook.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 1,
+    appName: 'Messenger',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-messenger.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 2,
+    appName: 'WhatsApp',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-whatsapp.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 3,
+    appName: 'Instagram',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-instagram.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 4,
+    appName: 'Snapchat',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-snapchat.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 5,
+    appName: 'Twitter',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-twitter.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 6,
+    appName: 'Pinterest',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-pinterest.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 7,
+    appName: 'WeChat',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-wechat.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 8,
+    appName: 'LinkedIn',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-linkedin.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 9,
+    appName: 'Telegram',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/social-telegram.png',
+    category: 'SOCIAL',
+    price: "199$",
+  },
+  {
+    appId: 10,
+    appName: 'Subway Surfers',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-subway-surfers.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 11,
+    appName: 'Crossy Road',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-crossy-road.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 12,
+    appName: 'Super Chef',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-super-chef.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 13,
+    appName: 'Angry Birds',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-angry-birds.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 14,
+    appName: 'Hill Climb 2',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-hill-climb-2.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 15,
+    appName: 'Temple Run',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-temple-run.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 16,
+    appName: 'Dr. Driving',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-dr-driving.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 17,
+    appName: 'Smurfs Bubble',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-smurfs-bubble.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 18,
+    appName: 'Grade Learning',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-grade-learning.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 19,
+    appName: 'My Talking Tom',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/games-my-talking-tom.png',
+    category: 'GAMES',
+    price: "199$",
+  },
+  {
+    appId: 20,
+    appName: 'Inshorts',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-inshorts.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 21,
+    appName: 'Way2News',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-way2news.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 22,
+    appName: 'Google News',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-google-news.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 23,
+    appName: 'Flipboard',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-flipboard.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 24,
+    appName: 'SmartNews',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-smart-news.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 25,
+    appName: 'BBC News',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-bbc-news.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 26,
+    appName: 'CNN News',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-cnn-news.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 27,
+    appName: 'Daily Wire',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-daily-wire.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 28,
+    appName: 'AP News',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-ap-news.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 29,
+    appName: 'News Break',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/news-news-break.png',
+    category: 'NEWS',
+    price: "199$",
+  },
+  {
+    appId: 30,
+    appName: 'Zomato',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-zomato.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 31,
+    appName: 'Swiggy',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-swiggy.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 32,
+    appName: "Domino's Pizza",
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-dominos.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 33,
+    appName: 'All in One',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-all-in-one.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 34,
+    appName: 'Instacart',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-insta-cart.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 35,
+    appName: 'Saucey',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-saucey.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 36,
+    appName: 'Waitr',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-waitr.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 37,
+    appName: 'Grubhub',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-grubhub.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 38,
+    appName: 'Mercato',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/app-store/food-mercato.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+  {
+    appId: 39,
+    appName: 'DOT',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/app-store/food-dot.png',
+    category: 'FOOD',
+    price: "199$",
+  },
+]
+
+
+class AppStore extends Component {
+  state = {activeTabId: tabsList[0].tabId, searchInput: ''}
+
+  getFilteredApps = () => {
+    const {activeTabId} = this.state
+    const filteredApps = appsList.filter(
+      eachApp => eachApp.category === activeTabId,
+    )
+    return filteredApps
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  getSearchResults = () => {
+    const {searchInput, activeTabId} = this.state
+    const searchResults = appsList.filter(
+      eachItem =>
+        eachItem.appName.toLowerCase().includes(searchInput.toLowerCase()) &&
+        eachItem.category === activeTabId,
+    )
+    return searchResults
+  }
+
+  onClickTabUpdate = value => {
+    this.setState({activeTabId: value})
+  }
+
+  render() {
+    const {activeTabId, searchInput} = this.state
+    const filteredAppsList = this.getFilteredApps()
+    const searchedAppsList = this.getSearchResults()
+
+    return (
+      <div className="bg-container">
+        <h1 className="heading">App Store</h1>
+        <div className="search-container">
+          <input
+            type="search"
+            className="search"
+            onChange={this.onChangeSearchInput}
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/app-store/app-store-search-img.png"
+            alt="search icon"
+            className="search-icon"
+          />
+        </div>
+        <ul className="tabs-container">
+          {tabsList.map(eachItem => (
+            <TabItem
+              tabDetails={eachItem}
+              key={eachItem.tabId}
+              onClickTabUpdate={this.onClickTabUpdate}
+              isActive={eachItem.tabId === activeTabId}
+            />
+          ))}
+        </ul>
+        <ul className="apps-container">
+          {searchedAppsList.map(eachItem => (
+            <AppItem appDetails={eachItem} key={eachItem.appId} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default AppStore
